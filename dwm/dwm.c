98bef03b8b376e3c534f38cfbbc7dba908206686
pbentes <pbentes@eckle.pt>
2023-08-07 19:24:31 +0100

    Warp Cursor Patch

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -839,6 +839,8 @@ focusmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+	if (selmon->sel)
+		XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
 }
 
 void
@@ -864,6 +866,7 @@ focusstack(const Arg *arg)
 	if (c) {
 		focus(c);
 		restack(selmon);
+		XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w/2, c->h/2);
 	}
 }
 
