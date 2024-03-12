fd2118784796ce9e1e746f035198814491be6521
Pedro Bentes <pedrogomes2000.3@gmail.com>
2024-03-12 22:04:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,37 +9,6 @@ sudo pacman -S --noconfirm --needed base-devel wget git
 mkdir -p ~/.config/dotfiles
 git clone https://github.com/pbentes/dotfiles.git ~/.config/dotfiles
 
-clear
-
-# choose video driver
-echo "1) xf86-video-intel 	2) xf86-video-amdgpu 3) nvidia 4) Skip"
-read -r -p "Choose you video card driver(default 1)(will not re-install): " vid
-
-case $vid in 
-[1])
-	DRI='xf86-video-intel'
-	;;
-
-[2])
-	DRI='xf86-video-amdgpu'
-	;;
-
-[3])
-	DRI='nvidia nvidia-settings nvidia-utils'
-    	;;
-
-[4])
-	DRI=""
-	;;
-[*])
-	DRI='xf86-video-intel'
-	;;
-esac
-
-# install xorg if not installed
-echo "Installing xorg"
-sudo pacman -S --noconfirm --needed xorg xorg-xinit xorg-xinput $DRI
-
 # install fonts
 echo "Installing fonts"
 mkdir -p ~/.local/share/fonts
@@ -50,6 +19,7 @@ clear
 
 # install suckless utils
 echo "Installing suckless programs"
+
 cd ~/.config/dotfiles/suckless/st
 sudo make clean install
 make clean
@@ -70,6 +40,9 @@ sudo make clean install
 make clean
 cd ~
 clear
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install AUR helper
 mkdir -p ~/.srcs
@@ -104,9 +77,6 @@ sudo chmod +x ~/.fehbg
 
 # install neovim
 sudo pacman -S --noconfirm neovim
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install picom
 $HELPER -S picom-ftlabs-git\
