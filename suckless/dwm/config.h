3602c0c489b6cc07f87772adac075e22073d9d6e
pbentes <pedrogomes2000.3@gmail.com>
2024-08-10 23:27:03 +0100

    Changed to gruvbox theme

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -8,15 +8,29 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ZedMono Nerd Font:size=10" };
 static const char dmenufont[]       = "ZedMono Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#56b6c2";
+static const char norm_fg[] = "#a89984";
+static const char norm_bg[] = "#282828";
+static const char norm_border[] = "#928374";
+
+static const char sel_fg[] = "#282828";
+static const char sel_bg[] = "#d65d0e";
+static const char sel_border[] = "#a89984";
+
+static const char urg_fg[] = "#a89984";
+static const char urg_bg[] = "#cc241d";
+static const char urg_border[] = "#b8bb26";
+
+static const char title_fg[] = "#b8bb26";
+static const char title_bg[] = "#3A3A3A";
+
+static const char col_borderbar[]   = "#75715e";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+    [SchemeTitle] = { title_fg,   title_bg,  norm_border },
 };
 
 /* tagging */
@@ -60,15 +74,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *obscmd[]  = { "flatpak", "run", "com.obsproject.Studio", NULL };
-static const char *chromiumcmd[]  = { "chromium", "--force-dark-mode", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_F1,     spawn,          {.v = obscmd } },
-	{ MODKEY,                       XK_F2,     spawn,          {.v = chromiumcmd } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
